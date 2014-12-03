d4392929307544548d472c2c9e54088a580266e9
TKogure <kogure@izmotion.co.jp>
2014-12-03 16:58:15 +0900

    sideBarAlert visibleBottom2

--- single.php
+++ single.php
@@ -19,9 +19,9 @@ jQuery(function(){
 		if(visibleBottom >= targetBottom) {
 			if(footerHeight > contentHeight - visibleBottom) {
 				target.css({position:"fixed", bottom: footerHeight - (contentHeight - visibleBottom)});
-				alert(visibleBottom);
 			} else {
 				target.css({position:"fixed"}).css({bottom: 0}).css({"margin-left": 774});
+				alert(visibleBottom);
 			}
 		} else {
 			target.css({position:"static", bottom: "auto" ,"margin-left":0});
